b57df4c5310107b8aa2b3fc0ca7bab777035bed5
ironfirst <Daniel_petersen_99@msn.com>
2020-12-14 17:40:30 +0100

    rep class setup

---
+++ src/main/java/com/example/brusha/util/DatabaseConnectionManager.java
@@ -0,0 +1,13 @@
+package com.example.brusha.util;
+
+import java.sql.Connection;
+
+public class DatabaseConnectionManager {
+    private static String user;
+    private static String password;
+    private static String url;
+    private static Connection conn;
+
+    public static Connection getDa
+
+}
